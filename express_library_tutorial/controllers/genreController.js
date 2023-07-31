16fe8c596b5043165b4e638abffc5ff71e68c7af
JustWaveThings <justwavethings@proton.me>
2023-07-31 18:37:52 -0400

    Create handlers for genre create and post forms

--- express_library_tutorial/controllers/genreController.js
+++ express_library_tutorial/controllers/genreController.js
@@ -1,6 +1,7 @@
 const Genre = require('../models/genre');
 const asyncHandler = require('express-async-handler');
 const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Genre.
 exports.genre_list = asyncHandler(async (req, res, next) => {
@@ -33,14 +34,35 @@ exports.genre_detail = asyncHandler(async (req, res, next) => {
 
 // Display Genre create form on GET.
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create GET');
+  res.render('layout', { title: 'Create Genre', errors: null, partial: 'genre_form' });
 });
 
 // Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Genre create POST');
-});
+exports.genre_create_post = [
+  body('name', 'Genre name required - at least 3 characters').trim().isLength({ min: 3 }).escape(),
 
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const genre = new Genre({ name: req.body.name });
+    if (!errors.isEmpty()) {
+      res.render('layout', { title: 'Create Genre', genre, errors: errors.array(), partial: 'genre_form' });
+      return;
+    } else {
+      const genreExists = await Genre.findOne({ name: req.body.name })
+       .collation({ locale: "en", strength: 2 })
+       .exec();
+       if (genreExists) {
+      // Genre exists, redirect to its detail page.
+      res.redirect(genreExists.url);
+      } else {
+      await genre.save();
+      // New genre saved. Redirect to genre detail page.
+      res.redirect(genre.url);
+    }
+    }
+  }),
+];
+  
 // Display Genre delete form on GET.
 exports.genre_delete_get = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Genre delete GET');
