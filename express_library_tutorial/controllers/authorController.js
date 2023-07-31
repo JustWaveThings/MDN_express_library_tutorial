fe27ec7e8ddba0a7bfe339013f77d972a283d128
JustWaveThings <justwavethings@proton.me>
2023-07-31 18:37:33 -0400

    Create handlers for author create and post forms

--- express_library_tutorial/controllers/authorController.js
+++ express_library_tutorial/controllers/authorController.js
@@ -1,6 +1,7 @@
 const Author = require('../models/author');
 const asyncHandler = require('express-async-handler');
 const Book = require("../models/book");
+const { body, validationResult } = require("express-validator");
 
 // Display list of all Authors.
 exports.author_list = asyncHandler(async (req, res, next) => {
@@ -33,13 +34,74 @@ exports.author_detail = asyncHandler(async (req, res, next) => {
 
 // Display Author create form on GET.
 exports.author_create_get = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create GET');
+  res.render("layout", { title: "Create Author", errors: null, partial: "author_form" });
 };
 
 // Handle Author create on POST.
-exports.author_create_post = (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Author create POST');
-};
+exports.author_create_post = [
+  // Validate and sanitize fields.
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("family_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Family name has non-alphanumeric characters."),
+  body("date_of_birth", "Invalid date of birth")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+  body("date_of_death", "Invalid date of death")
+    .optional({ checkFalsy: true })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("layout", {
+        title: "Create Author",
+        author,
+        errors: errors.array(),
+        partial: "author_form",
+      });
+      return;
+    } else {
+      const authorExists = await Author.findOne({
+        first_name: req.body.first_name,
+        family_name: req.body.family_name,
+      })
+        .collation({ locale: "en", strength: 2 })
+        .exec();
+      if (authorExists) {
+        // Author exists, redirect to its detail page.
+        res.redirect(authorExists.url);
+      } else {
+        await author.save();
+        // New author saved. Redirect to author detail page.
+        res.redirect(author.url);
+      }
+    }
+  }),
+
+]
 
 // Display Author delete form on GET.
 exports.author_delete_get = (req, res, next) => {
