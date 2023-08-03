b9ac113ba1db0fc2038bb12e6162b14e64d4b620
JustWaveThings <justwavethings@proton.me>
2023-08-03 08:32:27 -0400

    Add book delete template and handler

--- express_library_tutorial/controllers/bookController.js
+++ express_library_tutorial/controllers/bookController.js
@@ -113,12 +113,33 @@ exports.book_create_post = [
 
 // Display book delete form on GET.
 exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book delete GET');
+  const [book, bookInstances] = await Promise.all([
+    Book.findById(req.params.id).exec(),
+    BookInstance.find({ book: req.params.id }).exec(),
+  ]);
+  if (book == null) {
+    res.redirect('/catalog/books');
+  }
+  res.render('layout', {title: 'Delete Book', book, book_instances: bookInstances, partial: 'book_delete'});
 });
 
 // Handle book delete on POST.
 exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Book delete POST');
+  const [book, bookInstances] = await Promise.all([
+    Book.findById(req.body.bookid).exec(),
+    BookInstance.find({ book: req.body.bookid }).exec(),
+  ]);
+  if (bookInstances.length > 0) {
+    res.render('layout', {
+      title: 'Delete Book',
+      book,
+      book_instances: bookInstances,
+      partial: 'book_delete', 
+    });
+  } else {
+    await Book.findByIdAndRemove(req.body.bookid);
+    res.redirect('/catalog/books');
+  }
 });
 
 // Display book update form on GET.
