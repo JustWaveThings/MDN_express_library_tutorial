6fbaa546a2c3dd2bc9822dcdc0bb81ad6bdf9f8c
JustWaveThings <justwavethings@proton.me>
2023-08-03 07:34:03 -0400

    Create book instance delete template, update delete get/post handler

--- express_library_tutorial/controllers/bookInstanceController.js
+++ express_library_tutorial/controllers/bookInstanceController.js
@@ -69,12 +69,17 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete GET');
+  const bookInstance = await BookInstance.findById(req.params.id).populate('book').exec();
+  if (bookInstance === null) {
+    res.redirect("/catalog/bookinstances");
+  }
+  res.render('layout', {title: 'Delete Book Instance', book_instance: bookInstance, partial: 'book_instance_delete'});
 });
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+  await BookInstance.findByIdAndDelete(req.params.id);
+  res.redirect("/catalog/bookinstances");
 });
 
 // Display BookInstance update form on GET.
