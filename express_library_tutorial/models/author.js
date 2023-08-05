53fafd254aff6c3491b815b7d2248396587bca3e
JustWaveThings <justwavethings@proton.me>
2023-08-05 12:29:15 -0400

    Add date virutal props for form display

--- express_library_tutorial/models/author.js
+++ express_library_tutorial/models/author.js
@@ -37,6 +37,16 @@ AuthorSchema.virtual("date_of_death_formatted").get(function () {
     : "";
 });
 
+AuthorSchema.virtual('date_of_birth_yyyy_mm_dd').get(function () {
+  return this.date_of_birth 
+  ? DateTime.fromJSDate(this.date_of_birth).toISODate() : ""; 
+});
+
+AuthorSchema.virtual('date_of_death_yyyy_mm_dd').get(function () {
+  return this.date_of_birth 
+  ? DateTime.fromJSDate(this.date_of_death).toISODate() : "";
+});
+
 
 
 module.exports = mongoose.model('Author', AuthorSchema);
